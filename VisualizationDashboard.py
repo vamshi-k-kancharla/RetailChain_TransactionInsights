deb0f34e6699420edb1365d157c11284c5bf5a48
Vamshi Krishna Kancharla <vamshi.k.kancharla@gmail.com>
2026-03-20 22:49:09 +0530

    Retail Transaction Insights : Visualization Dashboard For Retail Transaction Insights

--- VisualizationDashboard.py
+++ VisualizationDashboard.py
@@ -12,7 +12,8 @@ def PrintSubTaskInputParameterDetails() :
     print("1 :=> Bar plot of no of transactions per city")
     print("2 :=> Pie chart of distribution of payment methods")
     print("3 :=> Line chart of monthly revenue trends")
-    print("4 :=> Exit the code")
+    print("4 :=> Stacked bar of revenue by Season and Customer Category")
+    print("5 :=> Exit the code")
 
     print('\n')
 
@@ -60,6 +61,67 @@ def PlotMonthlyRevenueTrends(retailTransactionsDataFrame) :
 
 
 '''
+    Stacked Bar of Total Revenue by Season & Customer Category
+'''
+
+def PlotStackedBarRevenueBySeasonAndCustomerCategory(retailTransactionsDataFrame) :
+
+    # learnt the stack bar from the source => https://www.geeksforgeeks.org/python/create-a-stacked-bar-plot-in-matplotlib/
+
+    # Extract Retail Transaction Total Cost for the Seasons & Customer Category
+
+    retailXnNewDF = retailTransactionsDataFrame[['Season', 'Customer_Category', 'Total_Cost']].groupby(['Season', 'Customer_Category']).sum()
+
+    retailXnStackBarDF = { 'Season' : [] }
+
+    customerCategories = []
+
+    # extract indexes of Season & Customer Category
+
+    for i, j in retailXnNewDF.index :
+
+        if i not in retailXnStackBarDF['Season'] :
+            retailXnStackBarDF['Season'].append(i)
+
+        if j not in customerCategories :
+
+            customerCategories.append(j)
+
+    #print(retailXnStackBarDF)
+    #print(customerCategories)
+
+
+    # Populate the Customer Category object values with Zeroes
+
+    for category in customerCategories :
+
+        retailXnStackBarDF[category] = []
+
+        for i in range(len(retailXnStackBarDF['Season'])) :
+
+            retailXnStackBarDF[category].append(0)
+
+    #print(retailXnStackBarDF)
+
+
+    # Now populate the DF object with actual values
+
+    for i, j in retailXnNewDF.index :
+
+        currentIndex = retailXnStackBarDF['Season'].index(i)
+
+        retailXnStackBarDF[j][currentIndex] = retailXnNewDF.loc[i,j]['Total_Cost']
+
+    retailXnStackBarDF
+
+    retailXnStackBarActualDF = pd.DataFrame(retailXnStackBarDF).set_index('Season')
+
+    retailXnStackBarActualDF.plot(kind='bar', stacked=True)
+
+    plt.show()
+
+
+'''
     Draw the plots & graphs for visualisation dashboard
 '''
 
@@ -83,6 +145,10 @@ def PlotTheTrendsAndCharts(retailTransactionsDataFrame) :
         case 3 :
 
             PlotMonthlyRevenueTrends(retailTransactionsDataFrame)
+
+        case 4 :
+
+            PlotStackedBarRevenueBySeasonAndCustomerCategory(retailTransactionsDataFrame)
 
         case _ :
 
